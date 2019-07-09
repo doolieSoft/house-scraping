2cb8b1a9b392f4670d845612958124f48cf1f5cf
Stefano <s.crapanzano@gmail.com>
2019-07-09 13:29:04 +0200

    import mark_safe

--- immo/annonce/models.py
+++ immo/annonce/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-from django.utils.safestring import mark_safe
-from datetime import datetime
 from django.utils import timezone
+from django.utils.safestring import mark_safe
+
 
 # Create your models here.
 class Annonce(models.Model):
